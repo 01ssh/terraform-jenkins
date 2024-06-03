f71bc8019164bee8dc137e1093e90483872060ef
Ubuntu <ubuntu@ip-172-31-25-159.eu-west-1.compute.internal>
2024-06-03 13:35:20 +0000

    try

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "eu-west-3"
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+   access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
